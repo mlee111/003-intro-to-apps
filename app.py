11383cde5e18e5079c20a41ef462e172daa4c565
mlee111 <michelle_lee1@intuit.com>
2022-07-12 23:30:49 +0000

    updating bar chart

--- app.py
+++ app.py
@@ -6,45 +6,31 @@ from dash import dcc
 import plotly.graph_objs as go
 
 ########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='darkred'
-color2='orange'
-mytitle='Beer Comparison'
+distributors=['Paramount', 'Disney', 'Warner Bros', 'Universal', 'Sony']
+dist_movies=[3, 3, 1, 2, 1]
+color='blue'
+mytitle='Highest grossing movies of 2022'
 
-label1='IBU'
-label2='ABV'
+label='by distributor'
 
 ########### Set up the chart
 
-def make_that_cool_barchart(beers, ibu_values, abv_values, color1, color2, mytitle):
-    bitterness = go.Bar(
-        x=beers,
-        y=ibu_values,
-        name=label1,
-        marker={'color':color1}
-    )
-    alcohol = go.Bar(
-        x=beers,
-        y=abv_values,
-        name=label2,
-        marker={'color':color2}
+def make_that_cool_barchart(distributors, dist_movies, color, label, mytitle):
+    dist = go.Bar(
+        x=distributors,
+        y=dist_movies,
+        name=label,
+        marker={'color':color}
     )
 
-    beer_data = [bitterness, alcohol]
-    beer_layout = go.Layout(
-        barmode='group',
-        title = mytitle
-    )
 
-    beer_fig = go.Figure(data=beer_data, layout=beer_layout)
-    return beer_fig
+    movie_fig = go.Figure(data=dist)
+    return movie_fig
 
 
 ######### Run the function #######
 
 if __name__ == '__main__':
-    fig = make_that_cool_barchart(beers, ibu_values, abv_values, color1, color2, mytitle)
+    fig = make_that_cool_barchart(distributors, dist_movies, color, label, mytitle)
     fig.write_html('docs/barchart.html')
     print('We successfully updated the barchart!')
